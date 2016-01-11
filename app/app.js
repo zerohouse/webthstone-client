924f4e93de3f6230a3070a523d7ffcb8e8d09488
park <parksungho86@gmail.com>
2016-01-11 22:50:30 +0900

    context change

---
+++ app/app.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+    angular.module('ws', [
+        'ngAnimate',
+        'ngCookies',
+        'ngMaterial',
+        'ngMessages',
+        'ngResource',
+        'ngSanitize',
+        'ngTouch',
+        'ui.router',
+        'ngFileUpload',
+        'toastr',
+        'anim-in-out'
+        /* @ngInject */
+    ]).run(function ($rootScope, $state, $window, $location) {
+        /* @ngInject */
+    }).config(function ($locationProvider, toastrConfig, $httpProvider, $mdThemingProvider) { //RestangularProvider,
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+        //RestangularProvider.setBaseUrl('/api/v1/');
+        angular.extend(toastrConfig, {
+            timeOut: 3000,
+            closeButton: true
+        });
+        $mdThemingProvider.theme('default');
+        //.primaryPalette('pink')
+        //.accentPalette('orange');
+    });
+}());
+
