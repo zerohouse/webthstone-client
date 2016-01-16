9f88a0f07893de1ce41ea4eae81f1afc0357815e
zerohouse <pakrusngho86@gmail.com>
2016-01-16 19:43:04 +0900

    카드사용 템플릿 수정

--- app/game.card/card.controller.js
+++ app/game.card/card.controller.js
@@ -3,7 +3,7 @@ ws.controller('cardDirectiveController', function ($scope, JSocket) {
 
     var card = $scope.card;
     $scope.submit = function () {
-        JSocket.send('card.submit', card.id);
+        JSocket.send('card.submit', {id: card.id});
     };
 
 });
