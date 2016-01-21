af3a3ae6ac4d6a64083bd1bc9f7acc05affdbaf1
dev <parksungho86@gmail.com>
2016-01-21 12:36:19 +0900

    레인지 추가

--- app/game/effect/effect.service.js
+++ app/game/effect/effect.service.js
@@ -12,11 +12,125 @@ ws.service('effect', function (http) {
     this.timings.enemy_die = "적 하수인이 죽을 때";
     this.timings.enemy_birth = "적 하수인이 뽑을 때";
 
+    this.rangeNames = {
+        "ALL_FIELD_FIGHTERS": "모든 필드 하수인",
+        "ALL_FIGHTERS": "모든 캐릭터",
+        "ENEMY_FIELD_FIGHTER": "적 필드 하수인",
+        "ENEMY_FIGHTER": "적 캐릭터",
+        "MY_FIELD_FIGHTERS": "내 필드 하수인",
+        "MY_FIGHTERS": "내 캐릭터",
+        "BOTH_HERO": "모든 영웅",
+        "MY_HERO": "내 영웅",
+        "ENEMY_HERO": "적 영웅"
+    };
+
     var self = this;
 
-    http.get('/api/card/effect').then(function (effects) {
-        self.effects = effects;
-    });
+    //http.get('/api/card/effect').then(function (effects) {
+    //    self.effects = effects;
+    //});
+
+    this.effects = {
+        "rand_target_assinate": {
+            "id": "rand_target_assinate",
+            "name": "무작위 타겟 제거",
+            "params": [
+                "제거 수"
+            ],
+            "rangeSelect": true,
+            "range": [
+                "ALL_FIELD_FIGHTERS",
+                "MY_FIELD_FIGHTERS",
+                "ENEMY_FIELD_FIGHTER"
+            ]
+        },
+        "manaadd": {
+            "id": "manaadd",
+            "name": "마나획득",
+            "params": [
+                "마나량"
+            ],
+            "rangeSelect": false,
+            "range": null
+        },
+        "rand_amount_attack": {
+            "id": "rand_amount_attack",
+            "name": "무작위 타겟(혹은 영웅지정) 무작위 피해(치료)",
+            "params": [
+                "최소 횟수",
+                "최대 횟수",
+                "최소 피해",
+                "최대 피해"
+            ],
+            "rangeSelect": true,
+            "range": [
+                "ALL_FIELD_FIGHTERS",
+                "ALL_FIGHTERS",
+                "ENEMY_FIELD_FIGHTER",
+                "ENEMY_FIGHTER",
+                "MY_FIELD_FIGHTERS",
+                "MY_FIGHTERS",
+                "BOTH_HERO",
+                "MY_HERO",
+                "ENEMY_HERO"
+            ]
+        },
+        "rand_attack": {
+            "id": "rand_attack",
+            "name": "무작위 타겟(혹은 영웅지정) 피해(치료)",
+            "params": [
+                "반복횟수",
+                "피해량"
+            ],
+            "rangeSelect": true,
+            "range": [
+                "ALL_FIELD_FIGHTERS",
+                "ALL_FIGHTERS",
+                "ENEMY_FIELD_FIGHTER",
+                "ENEMY_FIGHTER",
+                "MY_FIELD_FIGHTERS",
+                "MY_FIGHTERS",
+                "BOTH_HERO",
+                "MY_HERO",
+                "ENEMY_HERO"
+            ]
+        },
+        "rand_many_target_assinate": {
+            "id": "rand_many_target_assinate",
+            "name": "무작위 타겟 여러번 제거",
+            "params": [
+                "최소 제거",
+                "최대 제거"
+            ],
+            "rangeSelect": true,
+            "range": [
+                "ALL_FIELD_FIGHTERS",
+                "MY_FIELD_FIGHTERS",
+                "ENEMY_FIELD_FIGHTER"
+            ]
+        },
+        "manaadd_rand": {
+            "id": "manaadd_rand",
+            "name": "랜덤 마나 획득",
+            "params": [
+                "최소마나",
+                "최대마나"
+            ],
+            "rangeSelect": false,
+            "range": null
+        },
+        "target_assinate": {
+            "id": "target_assinate",
+            "name": "타겟 제거",
+            "params": [],
+            "rangeSelect": true,
+            "range": [
+                "ALL_FIELD_FIGHTERS",
+                "MY_FIELD_FIGHTERS",
+                "ENEMY_FIELD_FIGHTER"
+            ]
+        }
+    };
 
     this.Effect = Effect;
 
